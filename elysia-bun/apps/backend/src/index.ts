8aec43fc62e536273600dd410dff27689e57a0b1
Muhammad Zahid Masruri <masruri03@gmail.com>
2024-12-28 22:07:56 +0700

    feat(elysia-bun): :sparkles: complete elysia tutorial

--- elysia-bun/apps/backend/src/index.ts
+++ elysia-bun/apps/backend/src/index.ts
@@ -1,7 +1,23 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, error, t } from "elysia";
+import { note } from "./note";
+import { user } from "./user";
+import { opentelemetry } from "@elysiajs/opentelemetry";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(opentelemetry())
+	.use(swagger())
+	.onError(({ error, code }) => {
+		if (code === "NOT_FOUND") return "Not Found :(";
+
+		console.error(error);
+	})
+	.get("/", ({ path }) => "Hello Elysia from " + path)
+	.get("/hello", "Do you miss me?")
+	.use(note)
+	.use(user)
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
